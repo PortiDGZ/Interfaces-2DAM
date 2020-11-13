ef5368833aafc8c24420de2cd6cb07ff5dd77563
PortiDGZ <55698790+PortiDGZ@users.noreply.github.com>
2020-11-13 13:42:59 +0100

    Initial commit

---
+++ ConexionBBDD/Form1.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexionBBDD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public MySqlConnection conexion { get; private set; }
+
+        private DataTable comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            using (var cn = new SqlConnection(ConfigurationManager.ConnectionStrings["default"].ToString()))
+            {
+                using (var da = new SqlDataAdapter())
+                {
+                    using (var cmd = cn.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT CategoryID, CategoryName FROM Categories";
+                        da.SelectCommand = cmd;
+                        var dt = new DataTable();
+                        da.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ insertalumno/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace insertalumno
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionString = "Server=127.0.0.1;Database=interfaces; Uid=root;Pwd=root;";
+            conexion.Open();
+
+            string st = "INSERT INTO alumnos(id_alumno, alumno)VALUES('" + textBox1.Text + "'," + textBox2.Text + ")";
+
+            MySqlCommand comando = new MySqlCommand(st, conexion);
+            comando.ExecuteNonQuery();
+            conexion.Close();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Refresh();
+        }
+    }
+}
