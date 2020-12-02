440cf6a50e7d4ca468fd29ed39e05afb1d6ed56c
PortiDGZ <55698790+PortiDGZ@users.noreply.github.com>
2020-12-02 14:16:52 +0100

    harto

---
+++ ProyectoFinalFinal/Articulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProyectoFinalFinal
+{
+    public partial class Articulo : Form
+    {
+        public Articulo()
+        {
+            InitializeComponent();
+        }
+
+        private void Articulo_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'art_presuDataSet.articulo' Puede moverla o quitarla según sea necesario.
+            this.articuloTableAdapter.Fill(this.art_presuDataSet.articulo);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionString = "Server=localhost;Port=3306;Database=art_presu;Uid=root;Pwd=root;";
+            conexion.Open();
+
+            
+            MySqlCommand comando = new MySqlCommand();
+
+            comando.CommandText = "INSERT INTO room(ID,Nombre, Precio, Familia) VALUES(?id,?precio,?articulo,?familia)";
+            comando.Parameters.AddWithValue("?id", MySqlDbType.Int32).Value = Convert.ToInt32(textBox1.Text);
+            comando.Parameters.AddWithValue("?precio", MySqlDbType.VarChar).Value = Convert.ToInt32(textBox2.Text);
+            comando.Parameters.AddWithValue("?articulo", MySqlDbType.VarChar).Value = textBox3.Text;
+            comando.Parameters.AddWithValue("?familia", MySqlDbType.VarChar).Value = textBox4.Text;
+
+            comando.ExecuteNonQuery();
+            conexion.Close();
+
+
+        }
+    }
+}
--- insertalumno/Form2.Designer.cs
+++ insertalumno/Form2.Designer.cs
@@ -30,16 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.municipiosBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.interfacesDataSet = new insertalumno.interfacesDataSet();
-            this.municipiosTableAdapter = new insertalumno.interfacesDataSetTableAdapters.municipiosTableAdapter();
-            this.tableAdapterManager = new insertalumno.interfacesDataSetTableAdapters.TableAdapterManager();
-            this.alumnosBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.interfacesDataSet1 = new insertalumno.interfacesDataSet1();
-            this.alumnosTableAdapter = new insertalumno.interfacesDataSet1TableAdapters.alumnosTableAdapter();
-            this.tableAdapterManager1 = new insertalumno.interfacesDataSet1TableAdapters.TableAdapterManager();
             this.idalumnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.apellidosDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -47,11 +37,21 @@
             this.provinciaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.poblaciónDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.fechanacimientoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.alumnosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.interfacesDataSet1 = new insertalumno.interfacesDataSet1();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.municipiosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.interfacesDataSet = new insertalumno.interfacesDataSet();
+            this.municipiosTableAdapter = new insertalumno.interfacesDataSetTableAdapters.municipiosTableAdapter();
+            this.tableAdapterManager = new insertalumno.interfacesDataSetTableAdapters.TableAdapterManager();
+            this.alumnosTableAdapter = new insertalumno.interfacesDataSet1TableAdapters.alumnosTableAdapter();
+            this.tableAdapterManager1 = new insertalumno.interfacesDataSet1TableAdapters.TableAdapterManager();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.municipiosBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.interfacesDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.alumnosBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.interfacesDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.municipiosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.interfacesDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -67,73 +67,10 @@
             this.poblaciónDataGridViewTextBoxColumn,
             this.fechanacimientoDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.alumnosBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(70, 195);
+            this.dataGridView1.Location = new System.Drawing.Point(62, 195);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(652, 234);
+            this.dataGridView1.Size = new System.Drawing.Size(750, 234);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(313, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Insertar datos";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(633, 166);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(62, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Recargar";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // municipiosBindingSource
-            // 
-            this.municipiosBindingSource.DataMember = "municipios";
-            this.municipiosBindingSource.DataSource = this.interfacesDataSet;
-            // 
-            // interfacesDataSet
-            // 
-            this.interfacesDataSet.DataSetName = "interfacesDataSet";
-            this.interfacesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // municipiosTableAdapter
-            // 
-            this.municipiosTableAdapter.ClearBeforeFill = true;
-            // 
-            // tableAdapterManager
-            // 
-            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
-            this.tableAdapterManager.municipiosTableAdapter = this.municipiosTableAdapter;
-            this.tableAdapterManager.provinciasTableAdapter = null;
-            this.tableAdapterManager.UpdateOrder = insertalumno.interfacesDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
-            // 
-            // alumnosBindingSource
-            // 
-            this.alumnosBindingSource.DataMember = "alumnos";
-            this.alumnosBindingSource.DataSource = this.interfacesDataSet1;
-            // 
-            // interfacesDataSet1
-            // 
-            this.interfacesDataSet1.DataSetName = "interfacesDataSet1";
-            this.interfacesDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // alumnosTableAdapter
-            // 
-            this.alumnosTableAdapter.ClearBeforeFill = true;
-            // 
-            // tableAdapterManager1
-            // 
-            this.tableAdapterManager1.alumnosTableAdapter = this.alumnosTableAdapter;
-            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
-            this.tableAdapterManager1.municipiosTableAdapter = null;
-            this.tableAdapterManager1.provinciasTableAdapter = null;
-            this.tableAdapterManager1.UpdateOrder = insertalumno.interfacesDataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
             // idalumnoDataGridViewTextBoxColumn
             // 
@@ -177,11 +114,74 @@
             this.fechanacimientoDataGridViewTextBoxColumn.HeaderText = "fecha_nacimiento";
             this.fechanacimientoDataGridViewTextBoxColumn.Name = "fechanacimientoDataGridViewTextBoxColumn";
             // 
+            // alumnosBindingSource
+            // 
+            this.alumnosBindingSource.DataMember = "alumnos";
+            this.alumnosBindingSource.DataSource = this.interfacesDataSet1;
+            // 
+            // interfacesDataSet1
+            // 
+            this.interfacesDataSet1.DataSetName = "interfacesDataSet1";
+            this.interfacesDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Insertar datos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(633, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Recargar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // municipiosBindingSource
+            // 
+            this.municipiosBindingSource.DataMember = "municipios";
+            this.municipiosBindingSource.DataSource = this.interfacesDataSet;
+            // 
+            // interfacesDataSet
+            // 
+            this.interfacesDataSet.DataSetName = "interfacesDataSet";
+            this.interfacesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // municipiosTableAdapter
+            // 
+            this.municipiosTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.municipiosTableAdapter = this.municipiosTableAdapter;
+            this.tableAdapterManager.provinciasTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = insertalumno.interfacesDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // alumnosTableAdapter
+            // 
+            this.alumnosTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager1
+            // 
+            this.tableAdapterManager1.alumnosTableAdapter = this.alumnosTableAdapter;
+            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager1.municipiosTableAdapter = null;
+            this.tableAdapterManager1.provinciasTableAdapter = null;
+            this.tableAdapterManager1.UpdateOrder = insertalumno.interfacesDataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(944, 516);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -189,10 +189,10 @@
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.municipiosBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.interfacesDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.alumnosBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.interfacesDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.municipiosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.interfacesDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
